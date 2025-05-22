29ba1637972f032b34001fb42dd4e2303252d45d
Ray <lhy131120@gmail.com>
2025-05-22 17:15:19 +0800

    update image path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-	image: {
-		remotePatterns: [{ protocol: "https" }],
-	},
+	site: "https://myblogray.netlify.app",
+	base: "/",
+	integrations: [image()],
 });
